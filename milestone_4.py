322727027e50bee1697f4824a46cecbc3e4ae9db
RachelWilkinson0110 <rachel.wilkinson51@yahoo.com>
2023-05-30 16:32:10 +0100

    milestone 4 updates

--- milestone_4.py
+++ milestone_4.py
@@ -1,16 +1,20 @@
 
 import random
+
 class Hangman:
 
 #initilaising attributes
     def __init__(self,word_list, num_lives=5):
         self.word_list=word_list
-        self.num_lives=num_lives
+        self.num_lives=5
 
-        self.word=word
+        self.word=random.choice(word_list)
         self.word_guessed='_'*len(self.word)
-        self.num_letters=num_letters
+        self.num_letters=self.word_guessed.count('_')
         self.list_of_guesses= []
+
+        print(f'The mystery word has {self.num_letters} letters - {self.word_guessed}')
+        
         
 #Creating methods
 
@@ -18,12 +22,12 @@ class Hangman:
         guess=guess.lower()
         if guess in self.word:
             print(f'Good guess, {guess} is in the word!')
-            for char in word:
-                if char==guess:
-                    self.word_guessed.append(char)
-                else:
-                    self.word_guessed.append('_')
-                return self.word_guessed
+            for i in range(0, len(self.word)):
+                if self.word[i]==guess:
+                    result=i
+                    print(result)
+                    self.word_guessed=self.word_guessed.replace(self.word_guessed[result], guess, 1)
+            print(self.word_guessed)
             self.num_letters = self.num_letters -1
         else:
             print(f"{guess} is not in the word. Please try again")
@@ -39,6 +43,11 @@ class Hangman:
             elif guess in self.list_of_guesses:
                 print("You already tried that letter")
             else:
-                check_guess(guess)
+                self.check_guess(guess)
                 self.list_of_guesses += guess
+                print(self.list_of_guesses)
 
+word_list=['apple','banana']
+test=Hangman(word_list,num_lives=5)
+
+test.ask_for_input()
